0875836f939ed2b928f761386beb81198a0ec064
Ernesto Cantu <ernesto.cantu@aleph5.com>
2015-04-17 17:21:31 -0500

    ajustes

--- CODE/Swing Tutorial/JFileChooserExample/src/jfilechooserexample/JFileChooserExample.java
+++ CODE/Swing Tutorial/JFileChooserExample/src/jfilechooserexample/JFileChooserExample.java
@@ -3,19 +3,24 @@ package jfilechooserexample;
 import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JPanel;
+import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+
+
 
 /**
  * JFileChooser example.
  * @author ernesto
  */
-public class JFileChooserExample extends JPanel implements ActionListener {
+public class JFileChooserExample extends JFrame implements ActionListener {
     
     private JFileChooser fileChooser;
     private JButton openButton;
@@ -28,8 +33,10 @@ public class JFileChooserExample extends JPanel implements ActionListener {
         initComponents();
     }
     
-    private void initComponents(){
+    public void initComponents(){
         fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON Files","json");
+        fileChooser.setFileFilter(filter);
         openButton = new JButton("Select");
         
         setPreferredSize(new Dimension(278, 179));
@@ -38,33 +45,35 @@ public class JFileChooserExample extends JPanel implements ActionListener {
         add(openButton);
         openButton.setBounds(85,145,100,25);
         openButton.addActionListener(this);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(()->{
+            new JFileChooserExample().setVisible(true);
+        });
+    }
+    
+    @Override
     public void actionPerformed(ActionEvent evt){
         if(evt.getSource() == openButton){
-            returnValue = fileChooser.showOpenDialog(null);
+            returnValue = fileChooser.showOpenDialog(this);
             if(returnValue == JFileChooser.APPROVE_OPTION){
-                file = fileChooser.getSelectedFile();
+                JSONObject json = new JSONObject();
                 
+                JSONParser parser = new JSONParser();
+                
+                file = fileChooser.getSelectedFile();
                 try{
-                    bufferedReader = new BufferedReader(new FileReader(file));
+                    json = (JSONObject) parser.parse(new FileReader(file));                    
                     
-                    while((currentLine = bufferedReader.readLine()) != null){
-                        System.out.println(currentLine);
-                    };
+                    System.out.println(json.get("firstName"));
                 }
-                catch(Exception error){
-                    error.printStackTrace();
+                catch(Exception exp){
+                    System.out.println(exp.toString());
                 }
             }
         }
-    }
-   
-
-    public static void main(String[] args) {
-        
-        JFileChooserExample fileChooser = new JFileChooserExample();
-        fileChooser.setVisible(true);
-    }
-    
+    }    
 }
