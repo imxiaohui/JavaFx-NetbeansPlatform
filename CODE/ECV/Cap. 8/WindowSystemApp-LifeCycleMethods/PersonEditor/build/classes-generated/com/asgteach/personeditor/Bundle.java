11d530ce960eb77e67b3cfaf783abc252e3341ba
Ernesto Cantu <ernesto.cantu@aleph5.com>
2015-09-23 15:54:40 -0500

    Avance hasta primera parte de 8.6

---
+++ CODE/ECV/Cap. 8/WindowSystemApp-LifeCycleMethods/PersonEditor/build/classes-generated/com/asgteach/personeditor/Bundle.java
@@ -0,0 +1,48 @@
+package com.asgteach.personeditor;
+/** Localizable strings for {@link com.asgteach.personeditor}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Open</i>
+     * @see OpenTopComponent
+     */
+    static String CTL_OpenAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OpenAction");
+    }
+    /**
+     * @return <i>Open Window</i>
+     * @see OpenTopComponent
+     */
+    static String CTL_OpenTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OpenTopComponent");
+    }
+    /**
+     * @return <i>Person</i>
+     * @see PersonTopComponent
+     */
+    static String CTL_PersonAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonAction");
+    }
+    /**
+     * @return <i>Person Window</i>
+     * @see PersonTopComponent
+     */
+    static String CTL_PersonTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonTopComponent");
+    }
+    /**
+     * @return <i>This is a Open window</i>
+     * @see OpenTopComponent
+     */
+    static String HINT_OpenTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_OpenTopComponent");
+    }
+    /**
+     * @return <i>This is a Person window</i>
+     * @see PersonTopComponent
+     */
+    static String HINT_PersonTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PersonTopComponent");
+    }
+    private void Bundle() {}
+}
---
+++ CODE/ECV/Cap. 8/WindowSystemApp-TopComponentClientProps/PersonEditor/build/classes-generated/com/asgteach/personeditor/Bundle.java
@@ -0,0 +1,48 @@
+package com.asgteach.personeditor;
+/** Localizable strings for {@link com.asgteach.personeditor}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Limited</i>
+     * @see LimitedTopComponent
+     */
+    static String CTL_LimitedAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_LimitedAction");
+    }
+    /**
+     * @return <i>Limited Window</i>
+     * @see LimitedTopComponent
+     */
+    static String CTL_LimitedTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_LimitedTopComponent");
+    }
+    /**
+     * @return <i>Person</i>
+     * @see PersonTopComponent
+     */
+    static String CTL_PersonAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonAction");
+    }
+    /**
+     * @return <i>Person Window</i>
+     * @see PersonTopComponent
+     */
+    static String CTL_PersonTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonTopComponent");
+    }
+    /**
+     * @return <i>This is a Limited window</i>
+     * @see LimitedTopComponent
+     */
+    static String HINT_LimitedTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_LimitedTopComponent");
+    }
+    /**
+     * @return <i>This is a Person window</i>
+     * @see PersonTopComponent
+     */
+    static String HINT_PersonTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PersonTopComponent");
+    }
+    private void Bundle() {}
+}
