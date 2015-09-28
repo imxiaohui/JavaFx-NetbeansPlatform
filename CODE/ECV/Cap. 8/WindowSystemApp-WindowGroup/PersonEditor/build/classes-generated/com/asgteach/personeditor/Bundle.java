57cbb2a46cf3b65df374305bf537e0e033e91765
Ernesto Cantu <ernesto.cantu@aleph5.com>
2015-09-28 15:30:47 -0500

    Avanzo en 8.8

---
+++ CODE/ECV/Cap. 8/WindowSystemApp-WindowGroup/PersonEditor/build/classes-generated/com/asgteach/personeditor/Bundle.java
@@ -0,0 +1,111 @@
+package com.asgteach.personeditor;
+/** Localizable strings for {@link com.asgteach.personeditor}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>MyMode</i>
+     * @see MyModeTopComponent
+     */
+    static String CTL_MyModeAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MyModeAction");
+    }
+    /**
+     * @return <i>MyMode Window</i>
+     * @see MyModeTopComponent
+     */
+    static String CTL_MyModeTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MyModeTopComponent");
+    }
+    /**
+     * @return <i>Open</i>
+     * @see OpenTopComponent
+     */
+    static String CTL_OpenAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OpenAction");
+    }
+    /**
+     * @return <i>Open Window</i>
+     * @see OpenTopComponent
+     */
+    static String CTL_OpenTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OpenTopComponent");
+    }
+    /**
+     * @return <i>Person</i>
+     * @see PersonTopComponent
+     */
+    static String CTL_PersonAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonAction");
+    }
+    /**
+     * @return <i>PersonOutput</i>
+     * @see PersonOutputTopComponent
+     */
+    static String CTL_PersonOutputAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonOutputAction");
+    }
+    /**
+     * @return <i>PersonOutput Window</i>
+     * @see PersonOutputTopComponent
+     */
+    static String CTL_PersonOutputTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonOutputTopComponent");
+    }
+    /**
+     * @return <i>PersonProperties</i>
+     * @see PersonPropertiesTopComponent
+     */
+    static String CTL_PersonPropertiesAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonPropertiesAction");
+    }
+    /**
+     * @return <i>PersonProperties Window</i>
+     * @see PersonPropertiesTopComponent
+     */
+    static String CTL_PersonPropertiesTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonPropertiesTopComponent");
+    }
+    /**
+     * @return <i>Person Window</i>
+     * @see PersonTopComponent
+     */
+    static String CTL_PersonTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PersonTopComponent");
+    }
+    /**
+     * @return <i>This is a MyMode window</i>
+     * @see MyModeTopComponent
+     */
+    static String HINT_MyModeTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_MyModeTopComponent");
+    }
+    /**
+     * @return <i>This is a Open window</i>
+     * @see OpenTopComponent
+     */
+    static String HINT_OpenTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_OpenTopComponent");
+    }
+    /**
+     * @return <i>This is a PersonOutput window</i>
+     * @see PersonOutputTopComponent
+     */
+    static String HINT_PersonOutputTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PersonOutputTopComponent");
+    }
+    /**
+     * @return <i>This is a PersonProperties window</i>
+     * @see PersonPropertiesTopComponent
+     */
+    static String HINT_PersonPropertiesTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PersonPropertiesTopComponent");
+    }
+    /**
+     * @return <i>This is a Person window</i>
+     * @see PersonTopComponent
+     */
+    static String HINT_PersonTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PersonTopComponent");
+    }
+    private void Bundle() {}
+}
