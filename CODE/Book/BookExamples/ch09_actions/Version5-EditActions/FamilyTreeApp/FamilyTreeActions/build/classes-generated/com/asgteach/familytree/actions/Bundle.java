7b80b8cab4e3808dd975efa696ddd232e810a548
Ernesto Cantu <ernesto.cantu@aleph5.com>
2015-10-22 17:16:46 -0500

    Avance cap 9

---
+++ CODE/Book/BookExamples/ch09_actions/Version5-EditActions/FamilyTreeApp/FamilyTreeActions/build/classes-generated/com/asgteach/familytree/actions/Bundle.java
@@ -0,0 +1,13 @@
+package com.asgteach.familytree.actions;
+/** Localizable strings for {@link com.asgteach.familytree.actions}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Refresh</i>
+     * @see RefreshAction
+     */
+    static String CTL_RefreshAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_RefreshAction");
+    }
+    private void Bundle() {}
+}
---
+++ CODE/Book/BookExamples/ch09_actions/Version5-EditActions/FamilyTreeApp/GroupViewer/build/classes-generated/com/asgteach/familytree/groupviewer/Bundle.java
@@ -0,0 +1,55 @@
+package com.asgteach.familytree.groupviewer;
+/** Localizable strings for {@link com.asgteach.familytree.groupviewer}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Group</i>
+     * @see GroupTopComponent
+     */
+    static String CTL_GroupAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_GroupAction");
+    }
+    /**
+     * @return <i>Group Window</i>
+     * @see GroupTopComponent
+     */
+    static String CTL_GroupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_GroupTopComponent");
+    }
+    /**
+     * @return <i>Group Node</i>
+     * @see GroupNode
+     */
+    static String HINT_GroupNode() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_GroupNode");
+    }
+    /**
+     * @return <i>This is a Group window</i>
+     * @see GroupTopComponent
+     */
+    static String HINT_GroupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_GroupTopComponent");
+    }
+    /**
+     * @return <i>Person</i>
+     * @see PersonNode
+     */
+    static String HINT_PersonNode() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PersonNode");
+    }
+    /**
+     * @return <i>Show all people</i>
+     * @see RootNode
+     */
+    static String HINT_RootNode() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_RootNode");
+    }
+    /**
+     * @return <i>People</i>
+     * @see RootNode
+     */
+    static String LBL_RootNode() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_RootNode");
+    }
+    private void Bundle() {}
+}
